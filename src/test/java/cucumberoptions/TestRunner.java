f500528efefe496a8ce126b5177ed3cc9ed3f4fd
Karan <saini.karan7@gmail.com>
2020-06-19 21:48:37 -0700

    19th June commit

--- src/test/java/cucumberoptions/TestRunner.java
+++ src/test/java/cucumberoptions/TestRunner.java
@@ -4,16 +4,15 @@ import org.junit.runner.RunWith;
 
 import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
-//import io.cucumber.testng.CucumberOptions;
-import io.cucumber.testng.AbstractTestNGCucumberTests;
+import cucumber.api.testng.AbstractTestNGCucumberTests;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(features="src/test/java/features", glue="stepdefinitions")
 
-/*public class TestRunner extends AbstractTestNGCucumberTests{
-
-}*/
-
-public class TestRunner {
+public class TestRunner extends AbstractTestNGCucumberTests{
 
 }
+
+/*public class TestRunner {
+
+}*/
